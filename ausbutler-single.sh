9a0ca4d1075f879b812cd8a1b55ab3a2c8e07c40
emkael <emkael@tlen.pl>
2020-10-08 23:58:30 +0200

    Multiple configs handling rewritten to use envsubst

--- ausbutler-single.sh
+++ ausbutler-single.sh
@@ -5,7 +5,15 @@ cd "$(dirname "$0")"
 
 echo "Processing $TOURNAMENT..."
 
-ln -sfn configs/config.$TOURNAMENT config
+set -o allexport
+source configs/_common.env
+source configs/$TOURNAMENT.env
+set +o allexport
+
+ls config.template | while read CONFIGFILE
+do
+    envsubst < config.template/$CONFIGFILE > config/$CONFIGFILE
+done
 python jfrteamy-ausbutler/butler.py calculate generate nowait
 
 echo "Done"
