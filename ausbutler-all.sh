0b3a1904bc9f3a27c93abd41eae09d20c1140aef
emkael <emkael@tlen.pl>
2020-10-15 23:00:06 +0200

    Commenting out ausbutler-all tournaments

--- ausbutler-all.sh
+++ ausbutler-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 cd "$(dirname "$0")"
-while read -r TOURNAMENT
+grep -v '^#' tournaments.txt | while read -r TOURNAMENT
 do
     ./ausbutler-single.sh $TOURNAMENT
-done < tournaments.txt
+done
